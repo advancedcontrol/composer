ee51e81af1312826c274ca639c471566d4948d5e
Stephen von Takach <steve@advancedcontrol.com.au>
2014-09-22 14:16:32 +1000

    remove old module instance

--- src/directives.js
+++ src/directives.js
@@ -259,6 +259,7 @@
                                 localWatch = null;
                                 execUnreg = null;
 
+                                delete $scope.coModuleInstance;
                                 delete $scope[oldLocal];
                                 oldLocal = null;
 
